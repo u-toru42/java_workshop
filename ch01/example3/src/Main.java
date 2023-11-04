55ab2c34e5078dfa616d74b553955241b48623d7
u-toru42 <golt7040@gmail.com>
2023-11-04 19:15:52 +0900

    p52まで

---
+++ ch01/example3/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String t = "This is text";
+        System.out.println("This is text");
+        System.out.println(t);
+
+        System.out.println("A");
+        System.out.println(53);
+        System.out.println(23.08f);
+        System.out.println(1.97);
+        System.out.println(true);
+    }
+}
