b971a88453cff7f4f4307213956de9eb9d50719b
u-toru42 <golt7040@gmail.com>
2023-11-07 21:10:42 +0900

    p71まで(Math.powを使用)

---
+++ ch01/Example05/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String t = "";
+        System.out.println(t);
+        t = t + "ジョー...";
+        System.out.println(t);
+        t = t + "joe ... went fishing";
+        System.out.println(t);
+    }
+}
